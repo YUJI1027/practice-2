2cf50d1642b0bb1e014c6264d510517be67aedbd
YUJI1027 <soka.miyara_6161@icloud.com>
2024-03-04 19:14:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+        <div className="container flex pop">
+          <a href="./Apps.js" className="logo">
+            <span>- Champions</span> RODO -
+          </a>
+          <nav>
+            <ul id="item-box"className="header_memu">
+              <li><a href="#training">種目管理</a></li>
+              <li><a href="#food">食事管理</a></li>
+              <li><a href="#movie">動画</a></li>
+              <li><a href="#contact">お問い合わせ</a></li>
+            </ul>
+          </nav>
+        </div>
       </header>
-    </div>
+
+      <main>
+        <section id="hero" className="container">
+          <div className="hero_inner">
+            <p className="pop">"When you hit a wall, that's when the real starting line emerges."</p>
+            <h1>壁にぶち当たったときこそ真のスタートライン</h1>
+          </div>
+          </section>
+
+          <section id="training" className="container">
+            <div className="heading">Training</div>
+            <h2 className="title pop">Training</h2>
+            <h3>何度も積み重ねた先に描いたビジョンが現実になるのをあなたは知っている</h3>
+            <div className="flex">
+              <div className="column-2">
+                <h4>- 日々のメニューを記録しよう! -</h4>
+                <a href="" target="_blank" className="btn">MORE</a>
+              </div>
+            </div>
+          </section>
+
+
+      </main>
+
+
+      </body>
   );
 }
 
